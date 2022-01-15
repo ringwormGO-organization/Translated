7c3d67c4914309e02930caed52e15538881739ac
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2022-01-15 16:55:42 +0100

    Linux example

---
+++ Translated-Win-Linux/compile.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo Compiling...
+
+g++ -o Example main.cpp
+
+echo Compiled
+
+echo Running binary...
+cls
+
+Example.exe
---
+++ Translated-Win-Linux/compile.sh
@@ -0,0 +1,7 @@
+echo Compiling
+
+g++ -o Example main.cpp && echo Compiled || echo FAILED to compile
+
+clear
+
+./Example
